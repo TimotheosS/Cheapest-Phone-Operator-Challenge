6245bdd3ea3feb49d2daa00b695057bf2c0e6542
TIMOTHEOS SOUROULLA <timotheos.souroulla@ericsson.com>
2021-12-01 18:03:19 +0100

    Read CSV and get an integer from user

---
+++ MainScript.py
@@ -0,0 +1,33 @@
+import csv
+import math
+
+# Read csv files for each operator
+file = open("OperatorA.csv")
+csvreader = csv.reader(file)
+header = next(csvreader)
+data_A = []
+for row in csvreader:
+    data_A.append(row)
+file.close()
+
+file = open("OperatorB.csv")
+csvreader = csv.reader(file)
+header = next(csvreader)
+data_B = []
+for row in csvreader:
+    data_B.append(row)
+file.close()
+
+# print(data_A[0][0])
+
+# Ask for an integer from the user through the terminal and repeat until the input is an actual integer
+got_int = False
+while (not(got_int)):
+    try:
+        num_given = int(input('Enter phone number in an integer form: '))
+        got_int = True 
+    except ValueError:
+        print('Please only input digits')
+
+# Find the lentgh of the given integer
+len_num = math.floor(math.log10(num_given))+1
