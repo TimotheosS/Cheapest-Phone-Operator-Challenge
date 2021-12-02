0d1052d79ea8148d9394a92a338559554438cfcd
TIMOTHEOS SOUROULLA <timotheos.souroulla@ericsson.com>
2021-12-02 16:12:07 +0100

    Added some comments on the main script

--- MainScript.py
+++ MainScript.py
@@ -1,7 +1,7 @@
 import csv
 import math
 
-#TODO: Add comments and try to improve
+#TODO: Add comments and try to improve and update ReadMe
 
 initial_prefix = 111111111 # A number that cannot be a prefix
 
@@ -53,12 +53,16 @@ counter = 0
 extracted = num_given // (10 ** (len_num - counter - 1))    # Get the first number of the integer given
 
 while (counter <= len_num) and (oper_A_Flag or oper_B_Flag):
+    # For checking if there is still a match between two iterations in the row
     count_A_previous = count_A_cur
     count_A_cur = 0
     count_B_previous = count_B_cur
     count_B_cur = 0
+
+    # add the next digit of the integer given in the variable extracted
     extracted = num_given // (10 ** (len_num - counter - 1))
 
+    # For Operator A
     for i in range(0,len(data_A)):
         len_A = math.floor(math.log10(int(data_A[i][0])))+1        
         if(len_A > counter):
@@ -75,6 +79,7 @@ while (counter <= len_num) and (oper_A_Flag or oper_B_Flag):
     elif((count_A_cur == 0) and (count_A_previous == 0)):        
         oper_A_Flag = False   
 
+    # For Operator B
     for i in range(0,len(data_B)):
         len_B = math.floor(math.log10(int(data_B[i][0])))+1        
         if(len_B > counter):
@@ -91,16 +96,23 @@ while (counter <= len_num) and (oper_A_Flag or oper_B_Flag):
     elif((count_B_cur == 0) and (count_B_previous == 0)):        
         oper_B_Flag = False   
 
+    # Increase counter that will help on extracting the next digit
     counter += 1
 
 if((prefix_A == initial_prefix) and (cost_A == -1) and (prefix_B == initial_prefix) and (cost_B == -1)):
+    # No match for Operator A and Operator B
     print("Unfortunatley, none of our operators can help you.")
 elif(prefix_A != initial_prefix and prefix_B == initial_prefix):
+    # Match for Operator A, but not Operator B
     print("Operator A will help you at the price of:",cost_A, "per minute")
 elif(prefix_A == initial_prefix and prefix_B != initial_prefix):
+    # Match for Operator B, but not Operator A
     print("Operator B will help you at the price of:",cost_B, "per minute")
 else:
+    # Match for both Operators
     if(cost_A <= cost_B):
+        # Operator A is cheaper
         print("Operator A will help you at the price of:",cost_A, "per minute")
     else:
+        # Operator B is cheaper
         print("Operator B will help you at the price of:",cost_B, "per minute")
